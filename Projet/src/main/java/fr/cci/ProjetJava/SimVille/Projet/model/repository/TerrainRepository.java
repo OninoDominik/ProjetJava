f788efe99e515bff87d5f5d1eacf49e527b5e289
unknown <zyroprime@laposte.net>
2019-05-22 14:04:30 +0200

    correction jparepo to crud repo pour terrain repo

--- Projet/src/main/java/fr/cci/ProjetJava/SimVille/Projet/model/repository/TerrainRepository.java
+++ Projet/src/main/java/fr/cci/ProjetJava/SimVille/Projet/model/repository/TerrainRepository.java
@@ -3,6 +3,6 @@ import fr.cci.ProjetJava.SimVille.Projet.model.Terrain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TerrainRepository extends JpaRepository<Terrain, Integer> {
+public interface TerrainRepository extends CrudRepository<Terrain, Integer> {
 
 }
